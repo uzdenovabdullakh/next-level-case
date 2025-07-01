8cf0ab6669048adafc170ecc878e9b6c51d6a9e5
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-07-01 16:04:26 +0300

    feat: add logger and validation pipe in main

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+  
+  await app.listen(port);
+  
+  logger.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
