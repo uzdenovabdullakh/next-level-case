d5ab580aaf852896e65717aab1ed3bd80d33745b
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-07-01 18:12:51 +0300

    feat: create health check api

---
+++ api/src/controllers/health.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Logger } from '@nestjs/common';
+import { HealthCheckService, HealthCheck, TypeOrmHealthIndicator, MicroserviceHealthIndicator } from '@nestjs/terminus';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('health')
+export class HealthController {
+  private readonly logger = new Logger(HealthController.name);
+
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+    private microservice: MicroserviceHealthIndicator,
+    private configService: ConfigService,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  async check() {
+    this.logger.log('Performing health check');
+    return this.health.check([
+      () => this.db.pingCheck('database', { timeout: 1500 }),
+      () =>
+        this.microservice.pingCheck('rabbitmq', {
+          transport: Transport.RMQ,
+          options: {
+            urls: [this.configService.get<string>('RABBITMQ_URL')],
+            queue: this.configService.get<string>('RABBITMQ_QUEUE_NAME'),
+          },
+        }),
+    ]);
+  }
+}
