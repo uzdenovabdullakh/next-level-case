08e405947b434f77608335f80a7e7317304f92a0
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-07-01 16:05:28 +0300

    feat: update app module by ConfigModule and TypeOrmModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './modules/tasks.module';
+import { Task } from './models/task.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [Task],
+        synchronize: true, // NOTE: для тестового задания, на проде естественно миграции
+      }),
+      inject: [ConfigService],
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
